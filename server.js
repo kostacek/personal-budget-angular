04b53097794168f784f84e995264fc540a37e8db
kostacek <65678243+kostacek@users.noreply.github.com>
2020-10-06 21:45:10 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,36 @@ const port = 3000;
 
 app.use('/', express.static('public'));
 
-const budget = '/budget.json';
-budget.json = budget;
+const budget = {
+    myBudget:[
+    {
+    title: 'Eat out',
+    budget: 30
+},
+{
+    title: 'Rent',
+    budget: 350
+},
+{
+    title: 'Grocery',
+    budget: 90
+},
+{
+    title: 'Cable',
+    budget: 100
+},{
+    title: 'Internet',
+    budget: 200
+},{
+    title: 'Travel',
+    budget: 150
+},{
+    title: 'Childcare',
+    budget: 1200
+},
+
+    ]
+};
 app.get('/hello', (req, res)=>{
     res.send('Hello World!');
 });
