c54c05e304bcf5e8f07b879ea03d7c24b9d841f9
Kyser Family <kyserfamily@Kysers-MacBook-Pro.local>
2020-10-08 09:32:18 -0400

    cors server

--- server.js
+++ server.js
@@ -1,20 +1,50 @@
+// Budget API
+
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use('/', express.static('public'));
+app.use(cors());//should be modified for production
 
-const budget = '/budget.json';
-budget.json = budget;
-app.get('/hello', (req, res)=>{
-    res.send('Hello World!');
-});
+const budget = {
+    myBudget:[
+    {
+    title: 'Eat out',
+    budget: 30
+},
+{
+    title: 'Rent',
+    budget: 350
+},
+{
+    title: 'Grocery',
+    budget: 90
+},
+{
+    title: 'Cable',
+    budget: 100
+},
+{
+    title: 'Internet',
+    budget: 200
+},
+{
+    title: 'Travel',
+    budget: 150
+},
+{
+    title: 'Childcare',
+    budget: 1200
+},
+
+    ]
+};
 
 app.get('/budget',(req, res)=>{
     res.json(budget);
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`API Example app listening at http://localhost:${port}`)
 });
-
